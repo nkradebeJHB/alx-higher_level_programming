ff22aac4b2a5d79546a857a31406b7fb09686c73
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-12-05 15:47:16 -0800

    3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     if isinstabce(my_list, list):
         my_list.reverse()
         for i in my_list:
-            print('{:d}'.format(i))
+            print('{:d}'.format(my_list[i]))
