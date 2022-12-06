59e5336e5a4ae1537b11e54bed6894c7f93e84be
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-12-05 16:01:37 -0800

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    copy = ""
-    for i in my_string:
-        if i is not 'c' and i is not 'C':
-            copy += i
-    return (copy)
+    new_string = [x for x in my_string if x !='c' and x != 'C']
+    return ("".join(new_string))
