51e862cfbd5c724fec7663c251d06071d40a265a
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-12-05 16:36:52 -0800

    10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    multiples = []
+    for i in range(len(my_list)):
+        if my_lisy[i] % 2 == 0:
+            multiple.append(True)
+        else:
+            multiple.append(False)
+    return (multiples)
