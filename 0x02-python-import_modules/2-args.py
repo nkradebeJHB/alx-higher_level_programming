bb452ae768944f2dd6460432525cd90cdfdf52b1
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-12-07 15:22:57 -0800

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 def print_arg(argv):
     count = len(argv) - 1
-    if n == 0:
+    if count == 0:
         print("{:d} argument.".format(count))
         return
     else:
-        if n == 1:
+        if count == 1:
             print("{:d} argument:".format(count))
         else:
-            print("{:d} arguments:".format(n))
+            print("{:d} arguments:".format(count))
         i = 1
-        while i <= n:
+        while i <= count:
             print("{:d}: {:s}".format(i, argv[i]))
             i += 1
 
