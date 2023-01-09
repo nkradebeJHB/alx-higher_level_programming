2d8711b632d4f9206742dec134a8b5c9fa1578ea
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-01-09 12:52:44 -0800

    0 method lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Module with method lookup
+"""
+
+
+def lookup(obj):
+    """
+    Function that return a list of attributes and 
+    methods of an object.
+    """
+    return dir(obj)
