85e3163f3bb1290a67860978cf796400b657d021
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-01-10 11:35:15 -0800

    2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,5 +5,5 @@ def append_write(filename="", text=""):
     """function that appends a string at the end of a text
     file and returns the number of charecters"""
 
-    with    open(filename, 'a') as  f:
-        return  f.write(text)
+    with open(filename, 'a') as f:
+        return f.write(text)
