427518a35619dfb075bd837447d3164bef477c7f
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-01-10 10:42:20 -0800

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,6 @@
 
 def read_file(filename=""):
     """A function that reads a text file and prints it"""
-    with open(filename) as openfile:
+    with open(filename, "r", encoding="UTF-8") as openfile:
         text = openfile.read()
         print(text, end="")
