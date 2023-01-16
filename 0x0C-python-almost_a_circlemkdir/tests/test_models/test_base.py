fc74f270816e08bcf8cb32f00e51cfb7727d4fff
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-01-16 11:34:22 -0800

    first check

--- 0x0C-python-almost_a_circlemkdir/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circlemkdir/tests/test_models/test_base.py
@@ -2,5 +2,11 @@
 '''Module for Base unit tests.'''
 import unittest
 
+class TestBase(unittest.TestCase):
+    """Base class testing"""
+
+    def instantiation(self):
+        pass
+
 if __name__ == "__main__":
         unittest.main()
