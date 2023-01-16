e1bbc6800dd5410e87f810b78dd434f4f6ed2337
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-01-15 19:59:43 -0800

    3 first rectangle

--- 0x0C-python-almost_a_circlemkdir/models/base.py
+++ 0x0C-python-almost_a_circlemkdir/models/base.py
@@ -12,6 +12,6 @@ class Base:
         """class constructor."""
         if id is None:
             Base.__nb_objects += 1
-            self.if = Base.__nb_objects
+            self.id = Base.__nb_objects
         else:
             self.id = id
---
+++ 0x0C-python-almost_a_circlemkdir/models/rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+"""Module for Rectangle class."""
+from models.base import Base
+
+class Rectangle(Base):
+    """
+    A Rectangle class.
+    Args:
+        width: horizontal side of the rectangle.
+        height: vertical side of the rectangle.
+        x: horizontal point of the rectangle.
+        y: vertical side of the rectangle.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """rectangle constructor"""
+
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+        @property
+        def width(self):
+            """width of the rectangle."""
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            self.__width = value
+
+        @property
+        def height(self):
+            """height of the rectangle"""
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            self.__height = value
+
+        @property
+        def x(self):
+            return self.__x
+
+        @x.setter
+        def x(self, value):
+            self.__x = value
+
+        @property
+        def y(self):
+            return self.__y
+
+        @y.setter
+        def y(self, value):
+            self.__y = value
