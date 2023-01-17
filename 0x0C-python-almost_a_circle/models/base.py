e1950103a9deb9f48e31ad8aeeb87aef3641f52a
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-01-17 07:15:25 -0800

    fixing 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""module for Base class"""
+"""
+========================
+module for Base class
+========================
+"""
 
 class Base:
     """Class base
     Args:
         id: an interger.
     """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
         """class constructor."""
+
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
