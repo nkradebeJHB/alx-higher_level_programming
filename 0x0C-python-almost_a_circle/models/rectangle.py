dd0c171363f8400af63a78d8eaae669dd77d5174
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-01-17 09:00:27 -0800

    2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
-"""Module for Rectangle class."""
+"""
+========================================================
+Module for Rectangle class that inherits form class Base
+========================================================
+"""
+
 from models.base import Base
+
 
 class Rectangle(Base):
     """
@@ -29,30 +35,31 @@ class Rectangle(Base):
             return self.__width
 
         @width.setter
-        def width(self, value):
-            self.__width = value
+        def width(self, width):
+            self.__width = width
 
         @property
         def height(self):
             """height of the rectangle"""
-            return self.__height
+           
+           return self.__height
 
         @height.setter
-        def height(self, value):
-            self.__height = value
+        def height(self, height):
+            self.__height = height
 
         @property
         def x(self):
             return self.__x
 
         @x.setter
-        def x(self, value):
-            self.__x = value
+        def x(self, x):
+            self.__x = x
 
         @property
         def y(self):
             return self.__y
 
         @y.setter
-        def y(self, value):
-            self.__y = value
+        def y(self, y):
+            self.__y = y
