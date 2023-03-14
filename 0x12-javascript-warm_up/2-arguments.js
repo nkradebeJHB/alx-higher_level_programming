2741185e0ffba2169f0d8701514fa53e05761dbe
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-03-13 18:51:46 -0700

    ...

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,12 +3,13 @@
  * script that prints a message depending of the number
  * of arguments passed.
  */
+
 if (process.argv.length === 2) {
   console.log('No argument');
 }
 else if (process.argv.length === 3) {
   console.log('Argument found');
 }
-else (process.argv.length > 3) {
+else {
   console.log('Arguments found');
 }
