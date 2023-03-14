a4e049aba78b7b236325cf23272a327ef108ec78
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-03-14 09:56:22 -0700

    2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// A rectangle class with a constructor
+
+class Rectangle {
+  constructor (w, h) {
+    if ((parseInt(w) > 0 &&
+        (parseInt(h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
