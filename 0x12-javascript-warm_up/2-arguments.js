cbe8bad53da3af184063a578c918b0e386a62430
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-03-13 18:08:41 -0700

    0 fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /*
- * script that prints a message depending of the number 
+ * script that prints a message depending of the number
  * of arguments passed.
  */
 if (process.argv.length === 2) {
