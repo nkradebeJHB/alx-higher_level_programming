fb152392136de8b0d069c064b17737afc69108b6
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-03-13 18:13:42 -0700

    1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-/*
- * script that prints 3 lines
+/* script that prints 3 lines
  */
-console.log(“C is fun”);
-console.log(“Python is cool”);
-console.log(“JavaScript is amazing”);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
