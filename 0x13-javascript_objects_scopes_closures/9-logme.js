12591561d78ec2985aae12e4b6eb1b3c07aebbb3
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-03-14 11:34:11 -0700

    9

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// A function that prints the number of arguments
+// already printed and the new argument value.
+exports.logMe = function (item) {
+  this.times = (this.times || 0) + 1;
+  console.log(`${this.times - 1}: ${item}`);
+};
