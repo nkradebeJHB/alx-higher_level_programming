6cbad603128dc0e34fba420b42b1c32e75374829
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-03-13 18:44:20 -0700

    5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,8 +4,8 @@
  * <first argument converted in integer>
  * if the first argument can be converted to an integer:
  */
-const arg = process.argv[2];
-if (isNaN(parseInt(arg)){
+const arg = parseInt(process.argv[2]);
+if (isNaN(arg)) {
 	console.log('Not a number');
 } else {
 	console.log('My number: ' + arg);
