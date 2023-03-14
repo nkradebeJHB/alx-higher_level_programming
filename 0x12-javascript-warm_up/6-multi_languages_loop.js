ff36cc84c796b724c8a35554d09807672a2979be
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-03-13 19:01:01 -0700

    6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,7 +3,7 @@
  * a script that prints 3 lines: (like 1-multi_languages.js)
  * but by using an array of string and a loop
  */
-const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < strings.length; i++) {
   console.log(strings[i]);
 }
