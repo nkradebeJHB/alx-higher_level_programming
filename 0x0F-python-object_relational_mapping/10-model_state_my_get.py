61a5738f2a2c35d550a744583bac65bd1b6ed76a
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-03-22 05:57:22 -0700

    10 fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -6,6 +6,11 @@ passed as argument from the database hbtn_0e_6_usa.
 ===================================================
 """
 
+from sys import argv
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
 if __name__ == "__main__":
     engine = create_engine(
                 'mysql+mysqldb://{}:{}@localhost:3306/{}'
@@ -17,7 +22,7 @@ if __name__ == "__main__":
     state = session.query(State).\
         filter(State.name == argv[4])
     if state:
-        print("{}".format(state[0].id)).order_by(State.id).all()
+        print("{}".format(state[0].id))
     else:
         print("Not found")
     session.close()
