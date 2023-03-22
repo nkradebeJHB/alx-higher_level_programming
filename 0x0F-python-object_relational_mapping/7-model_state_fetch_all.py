d8885d218fe39354d97707d4a1d23f6eb1514437
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-03-22 04:51:22 -0700

    7 fix

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,9 +7,9 @@ database hbtn_0e_6_usa.
 """
 
 from sys import argv
+from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine(
