9db32e1838e3f7eecd25f1e48a1015bf68f88906
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-03-30 14:11:31 -0700

    0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -3,4 +3,4 @@
 # request to that URL, and displays the size 
 # of the body of the response
 
-curl -sI /dev/null | grep -i content-length | awk '{print $2}'
+curl -so /dev/null -w '%{size_download}\n' "$1"
