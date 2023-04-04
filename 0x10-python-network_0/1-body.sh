8ffdf902f3714786871c228b230c3e008335e9dc
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-04-04 04:42:05 -0700

    fixing 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a 
-# GET request to the URL, and displays the body 
-# of the response.
-
-curl -Ls "$1"
+# Bash script that takes in a URL, sends a GET request to the URL.
+curl -sL "$1" -X GET
