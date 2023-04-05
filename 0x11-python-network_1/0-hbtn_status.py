60e5903d02584c28dd9ea7e6296d31e8f613ff79
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-04-04 17:21:53 -0700

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,6 @@ from urllib import request
 
 with request.urlopen("https://intranet.hbtn.io/status") as response:
     res = response.read()
-
 print('Body response:')
 print('\t- type:', type(res))
 print('\t- content:', res)
