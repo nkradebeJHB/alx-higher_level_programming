90ebdf61f34c238b22777cf45efcb5ca77d079cc
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-04-04 18:43:20 -0700

    7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ a Python script that takes in a URL, sends a request
 to the URL and displays the body of the response."""
+import requests
+from sys import argv
 
 if __name__ == "__main__":
     res = requests.get(argv[1])
