8bec1b63376a4cbdc18f58693381f280a8f2226d
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-04-04 17:41:44 -0700

    2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+======================================================
+A Python script that takes in a URL and an email, 
+sends a POST request to the passed URL with the email 
+as a parameter, and displays the body of the response 
+(decoded in utf-8).
+======================================================
+"""
+from urllib import request, parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    data = parse.urlencode({"email": sys.argv[2]}).encode()
+    with request.urlopen(url, data) as response:
+        print(response.read().decode('utf-8'))
