e2a13fd3483ff26e0c4ba70becb2983338e9f71a
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-04-04 17:16:21 -0700

    0 and read me

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+==============================================================
+Python script that fetches https://alx-intranet.hbtn.io/status
+==============================================================
+"""
+from urllib import request
+
+with request.urlopen("https://intranet.hbtn.io/status") as response:
+    res = response.read()
+
+print('Body response:')
+print('\t- type:', type(res))
+print('\t- content:', res)
+print('\t- utf8 content:', res.decode('utf8'))
