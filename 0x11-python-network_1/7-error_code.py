a9ef54be26323603ca1c38aa48203f24a9225392
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-04-04 18:38:16 -0700

    7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" a Python script that takes in a URL, sends a request
+to the URL and displays the body of the response."""
+
+if __name__ == "__main__":
+    res = requests.get(argv[1])
+    if res.status_code > 400:
+        print("Error code:", res.status_code)
+    else:
+        print(res.text)
