816fadfc36aa0553af6749597237dec686d9ab6e
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-04-04 17:49:47 -0700

    2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""A Python script that takes in a URL and an email, 
-sends a POST request to the passed URL with the email 
-as a parameter, and displays the body of the response 
-(decoded in utf-8)."""
+""" Fetches header from url passed as arg with email data"""
 from urllib import request, parse
 import sys
 
