7aa5377a78c3362c688667d7fb5e033a885c0a37
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-04-26 15:33:04 -0700

    installed request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,6 @@
 const request = require('request');
 request
   .get(process.argv[2])
-  .on('res', function (res) {
-    console.log('code: ' + res.statusCode);
+  .on('response', function (response) {
+    console.log('code: ' + response.statusCode);
   });
