4c42bce332eeffd659e8ede4c74aa96cfe4f01ac
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-04-26 15:14:41 -0700

    0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* a script that reads and prints the content of a file.*/
+
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', function (error, data) {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log(data);
+	}
+});
